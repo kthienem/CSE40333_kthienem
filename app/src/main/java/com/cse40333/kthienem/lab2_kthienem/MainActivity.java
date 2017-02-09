5cfad011bd552da67cd155a9127b318ebe45617d
kthienem <kthienem@nd.edu>
2017-02-08 20:01:07 -0500

    Lab 3 Update

--- app/src/main/java/com/cse40333/kthienem/lab2_kthienem/MainActivity.java
+++ app/src/main/java/com/cse40333/kthienem/lab2_kthienem/MainActivity.java
@@ -2,12 +2,41 @@ package com.cse40333.kthienem.lab2_kthienem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private String[] teamNames = {"Ohio State", "Florida State", "Wake Forest", "Boston College",
+//            "North Carolina State", "Georgia Tech", "North Virginia", "Chicago State"};
+    private ArrayList<String[]> info = new ArrayList<String[]>(){{
+            String[] schools = {"Ohio State", "Florida State", "Wake Forest", "Boston College",
+            "North Carolina State", "Georgia Tech", "North Virginia", "Chicago State"};
+
+            String[] dates = {"Feb 9", "Feb 11", "Feb 12", "Feb 14", "Feb 18", "Feb 26", "March 4", "March 8"};
+
+            String[] logos = {"ohio_state","fsu","wake_forest","boston_college","ncstate","georgia_tech","virginia","michigan"};
+
+            for (int i = 0; i < schools.length; i++) {
+                String[] temp = {schools[i], dates[i], logos[i]};
+                add(temp);
+            }
+    }};
+    private ListAdapter scheduleAdapter;
+    private ListView scheduleListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Schedule");
+
+        scheduleAdapter = new ScheduleAdapter(this, info);
+        scheduleListView = (ListView) findViewById(R.id.scheduleListView);
+        scheduleListView.setAdapter(scheduleAdapter);
     }
 }
---
+++ app/src/main/java/com/cse40333/kthienem/lab2_kthienem/ScheduleAdapter.java
@@ -0,0 +1,42 @@
+package com.cse40333.kthienem.lab2_kthienem;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kris on 2/8/2017.
+ */
+
+public class ScheduleAdapter extends ArrayAdapter<String[]> {
+
+    ScheduleAdapter (Context context, ArrayList<String[]> schedule) {
+        super(context, R.layout.schedule_item, schedule);
+    }
+
+    public View getView (int position, View convertView, ViewGroup parent) {
+        LayoutInflater scheduleInflater = LayoutInflater.from(getContext());
+        View scheduleView = scheduleInflater.inflate(R.layout.schedule_item, parent, false);
+
+        String[] matchItem = getItem(position);
+        TextView teamName = (TextView) scheduleView.findViewById(R.id.teamName);
+        teamName.setText(matchItem[0]);
+
+        TextView gameDate = (TextView) scheduleView.findViewById(R.id.gameDate);
+        gameDate.setText(matchItem[1]);
+
+        ImageView teamLogo = (ImageView) scheduleView.findViewById(R.id.teamLogo);
+        String mDrawableName = matchItem[2];
+        int resID = getContext().getResources().getIdentifier(mDrawableName, "drawable", getContext().getPackageName());
+        teamLogo.setImageResource(resID);
+
+        return scheduleView;
+    }
+}
