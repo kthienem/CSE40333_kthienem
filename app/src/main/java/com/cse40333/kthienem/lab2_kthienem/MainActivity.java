47ae99ea23694d990efa4208ab0da3506ce35477
kthienem <kthienem@nd.edu>
2017-03-19 20:22:25 -0400

    Lab 6
    Implemented features for lab 6

--- app/src/main/java/com/cse40333/kthienem/lab2_kthienem/MainActivity.java
+++ app/src/main/java/com/cse40333/kthienem/lab2_kthienem/MainActivity.java
@@ -1,12 +1,21 @@
 package com.cse40333.kthienem.lab2_kthienem;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -20,17 +29,20 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Team> info = new ArrayList<>();
     private ListAdapter scheduleAdapter;
     private ListView scheduleListView;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Schedule");
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        setTitle("ND Athletics");
 
         MyCsvFileReader reader = new MyCsvFileReader(getApplicationContext());
         info = reader.readCsvFile(R.raw.schedule);
 
-        scheduleAdapter = new ScheduleAdapter(this, info);
+        scheduleAdapter = new ScheduleAdapter(getBaseContext(), info);
         scheduleListView = (ListView) findViewById(R.id.scheduleListView);
         scheduleListView.setAdapter(scheduleAdapter);
 
@@ -69,5 +81,88 @@ public class MainActivity extends AppCompatActivity {
         mBundle.putString("score2", homeScore.get(id));
 
         return mBundle;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int res_id = item.getItemId();
+
+        if (res_id == R.id.share) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Basketball Matches");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, gameSchedule());
+            startActivity(Intent.createChooser(shareIntent, "Share via"));
+        }
+        if (res_id == R.id.sync) {
+            Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator_layout), "Sync is not yet implemented", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            TextView text = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+            text.setTextColor(Color.WHITE);
+            snackbar.setAction("Try Again", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar altSnackbar = Snackbar.make(findViewById(R.id.coordinator_layout), "Wait for the next few labs.", Snackbar.LENGTH_LONG);
+                    View altSnackbarView = altSnackbar.getView();
+                    TextView text = (TextView) altSnackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                    text.setTextColor(Color.WHITE);
+                    altSnackbar.show();
+                }
+            });
+            snackbar.show();
+        }
+        if (res_id == R.id.settings) {
+            registerForContextMenu(findViewById(R.id.scheduleListView));
+            openContextMenu(findViewById(R.id.scheduleListView));
+            unregisterForContextMenu(findViewById(R.id.scheduleListView));
+        }
+        return true;
+    }
+
+    public String gameSchedule() {
+        StringBuilder gameString = new StringBuilder();
+
+        for (Team team: info) {
+            gameString.append(team.getTeamName() + ", ");
+            gameString.append(team.getGameDate() + ", ");
+            gameString.append("Purcell Pavilion\n");
+        }
+        gameString.deleteCharAt(gameString.length()-1);
+        return gameString.toString();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.floating_contextual_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int res_id = item.getItemId();
+
+        if (res_id == R.id.womens) {
+            //TODO: Implement in later lab
+        }
+        if (res_id == R.id.mens) {
+            //TODO: Implement in later lab
+        }
+        if (res_id == R.id.away) {
+            //TODO: Implement in later lab
+        }
+        if (res_id == R.id.home) {
+            //TODO: Implement in later lab
+        }
+        return false;
     }
 }
